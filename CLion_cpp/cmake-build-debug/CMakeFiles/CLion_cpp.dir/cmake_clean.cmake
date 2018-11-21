c4ff3f582d9815da5b8e3b1963dbc365f973f2b1
michaelwangbh <bwnanjolno@outlook.com>
2018-11-20 17:48:55 -0800

    try out different c++ stuff

---
+++ CLion_cpp/cmake-build-debug/CMakeFiles/CLion_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CLion_cpp.dir/main.cpp.o"
+  "CLion_cpp.pdb"
+  "CLion_cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLion_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- CLion_cpp/main.cpp
+++ CLion_cpp/main.cpp
@@ -8,29 +8,29 @@
 using namespace std;
 string solution(string parentheses) {
     // Type your solution here
-    stack<char> s; 
-    char tmp; 
+    stack<char> s;
+    char tmp;
     int left = 0;
     int len = parentheses.length();
-    
-    for (int i = 0; i < len; ++i) { 
-        if (parentheses[i] == '(') 
-        { 
-            s.push(parentheses[i]); 
+
+    for (int i = 0; i < len; ++i) {
+        if (parentheses[i] == '(')
+        {
+            s.push(parentheses[i]);
             continue;
         } else {
-            if (!s.empty()) {
+            try {
                 tmp = s.top();
                 s.pop();
                 if (tmp != '(') left++;
-            } else {
+            } catch (...) {
                 left++;
             }
         }
-    } 
+    }
     int right = static_cast<int> (s.size());
     parentheses = string(left, '(') + parentheses + string(right, ')');
-    return parentheses; 
+    return parentheses;
 }
 
 int main(){
