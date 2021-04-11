a3093c38184cdd9b6791f5afd5e48a98e63bbe0d
duckino <25713361+duckino@users.noreply.github.com>
2021-04-10 23:01:41 -0700

    add 329

---
+++ python/329.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        maxL = 0
+        m, n = len(matrix), len(matrix[0])
+        memo = [[0 for i in range(n)] for j in range(m)]
+
+        def dfs(i, j, m, n):
+            if memo[i][j] > 0:
+                return memo[i][j]
+            localmax = 0
+            for k in dirs:
+                x, y = i + k[0], j + k[1]
+                if x >= 0 and y >= 0 and x < m and y < n and matrix[i][j] < matrix[x][y]:
+                    localmax = max(localmax, dfs(x, y, m, n))
+            memo[i][j] = localmax + 1
+
+            return memo[i][j]
+
+        for i in range(m):
+            for j in range(n):
+                maxL = max(maxL, dfs(i, j, m, n))
+
+        return maxL
