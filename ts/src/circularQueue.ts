66a16fc7c1014f60ba957cc3e2435c9a49ca2d0b
byhow <baihaow@uci.edu>
2022-02-12 23:25:21 -0800

    chore: prettier

--- ts/src/circularQueue.ts
+++ ts/src/circularQueue.ts
@@ -1,69 +1,67 @@
-class ListNode{
-    val: number;
-    next: ListNode | null;
-    constructor(val: number, next=null) {
-        this.val = val;
-        this.next = next
-    }
+class ListNode {
+  val: number;
+  next?: ListNode;
+  constructor(val: number, next?: ListNode) {
+    this.val = val;
+    this.next = next;
+  }
 }
 
 class MyCircularQueue {
-    maxSize: number;
-    size: number;
-    head: ListNode | null;
-    tail: ListNode | null;
-    constructor(k: number) {
-        this.maxSize = k
-        this.size = 0
-        this.head = null
-        this.tail = null
+  maxSize: number;
+  size: number;
+  head?: ListNode;
+  tail?: ListNode;
+
+  constructor(k: number) {
+    this.maxSize = k;
+    this.size = 0;
+  }
+
+  enQueue(value: number): boolean {
+    if (this.isFull()) {
+      return false;
+    }
+    let newVal = new ListNode(value);
+    if (this.isEmpty()) {
+      this.head = this.tail = newVal;
+    } else {
+      if (this.tail) {
+        this.tail.next = newVal;
+        this.tail = this.tail?.next;
+      }
+    }
+    this.size++;
+    return true;
+  }
+
+  deQueue(): boolean {
+    if (this.isEmpty()) {
+      return false;
+    }
+    if (this.head) {
+      this.head = this.head.next;
     }
 
-    enQueue(value: number): boolean {
-        if (this.isFull()) {
-            return false
-        }
-        let newVal = new ListNode(value)
-        if (this.isEmpty()) {
-            this.head = this.tail = newVal
-        } else {
-            if (this.tail) {
-                this.tail.next = newVal
-                this.tail = this.tail?.next
-            }
-        }
-        this.size++
-        return true
-        
-    }
+    this.size--;
+    return true;
+  }
 
-    deQueue(): boolean {
-        if (this.isEmpty()) {
-            return false
-        } 
-        if (this.head) {
-            this.head = this.head.next
-        }
-        
-        this.size--
-        return true
-    }
+  front(): number {
+    return this.isEmpty() ? -1 : this.head ? this.head.val : -1;
+  }
 
-    Front(): number {
-        return this.isEmpty() ? -1 : this.head ? this.head.val : -1
-    }
+  rear(): number {
+    return this.isEmpty() ? -1 : this.tail ? this.tail.val : -1;
+  }
 
-    Rear(): number {
-        return this.isEmpty() ? -1 : this.tail ? this.tail.val : -1
-    }
+  isEmpty(): boolean {
+    return this.size === 0;
+  }
 
-    isEmpty(): boolean {
-        return (this.size === 0)
-    }
-
-    isFull(): boolean {
-        return (this.size === this.maxSize)
-    }
+  isFull(): boolean {
+    return this.size === this.maxSize;
+  }
 }
 
 /**
