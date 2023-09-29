4bcec8cdea8e4919ec9d849aa933cc44243bd5a4
byhow <by.how@hotmail.com>
2023-09-28 22:23:35 -0700

    feat: react stuff

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,30 +1,27 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const [randomUserJson, setRandomUserJson] = useState('');
+  const [randomUserJson, setRandomUserJson] = useState("");
 
   useEffect(() => {
-    fetchRandomUserData().then(res => setRandomUserJson(res));
-  }, [])
+    fetchRandomUserData().then((res) => setRandomUserJson(res));
+  }, []);
 
   return (
     <div className="App">
       <h1>Button</h1>
       <p>{counter}</p>
-      <button onClick={() => setCounter(counter+1)}>+</button>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
       <pre>{randomUserJson}</pre>
     </div>
   );
 }
 
-const fetchRandomUserData = () => {
-  return axios.get(`https://randomuser.me/api`)
-    .then(({data}) => {
-      return JSON.stringify(data);
-    });
-}
+const fetchRandomUserData = async () => {
+  const { data } = await axios.get(`https://randomuser.me/api`);
+  return JSON.stringify(data);
+};
 
 export default App;
