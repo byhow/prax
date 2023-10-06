7747a4596e5c428f2dad8486cc6cbdd7bc3eb0fa
byhow <by.how@hotmail.com>
2023-10-06 01:54:28 -0700

    feat: 132

---
+++ ts/src/132pattern.ts
@@ -0,0 +1,12 @@
+function find132pattern(nums: number[]): boolean {
+    let stack = [];
+    let third = Number.MIN_SAFE_INTEGER;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (nums[i] < third) return true;
+        while (stack.length && stack[stack.length - 1] < nums[i]) {
+            third = stack.pop()
+        }
+        stack.push(nums[i]);
+    }
+    return false
+};
